e1f4cc7ad671e6bb6406353e5115dfa15d768898
RainFool <rainfool1024@gmail.com>
2018-01-12 20:03:23 +0800

    引入RecyclerView界面

---
+++ app/src/main/java/com/example/app/recyclerview/RecyclerViewDemoActivity.java
@@ -0,0 +1,13 @@
+package com.example.app.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclerViewDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_demo);
+    }
+}
