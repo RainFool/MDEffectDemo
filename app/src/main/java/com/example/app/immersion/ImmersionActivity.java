c99ce4e1a4564e1575baef587515acbf2e93770d
RainFool <rainfool1024@gmail.com>
2018-02-07 11:39:49 +0800

    沉浸式尝试小米

--- app/src/main/java/com/example/app/immersion/ImmersionActivity.java
+++ app/src/main/java/com/example/app/immersion/ImmersionActivity.java
@@ -1,8 +1,12 @@
 package com.example.app.immersion;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
@@ -11,6 +15,9 @@ import android.widget.Button;
 
 import com.example.app.R;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * 沉浸式Demo
  *
@@ -18,9 +25,9 @@ import com.example.app.R;
  * @date 2017/12/8
  */
 
-public class ImmersionActivity extends AppCompatActivity {
+public class ImmersionActivity extends Activity {
 
-    Button mBtnTransparent,mBtnWhite;
+    Button mBtnTransparent, mBtnWhite;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,46 +37,57 @@ public class ImmersionActivity extends AppCompatActivity {
         mBtnTransparent = (Button) findViewById(R.id.btn_transparent);
         mBtnWhite = (Button) findViewById(R.id.btn_white);
 
-        mBtnTransparent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StatusBarUtil.setImmersionMode(ImmersionActivity.this,false);
-                changeToTransparent();
-            }
-        });
-
-        mBtnWhite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StatusBarUtil.setImmersionMode(ImmersionActivity.this,true);
-                changeToWhite();
-            }
-        });
-    }
-
-    private void changeToTransparent() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            final Window window = getWindow();
-            //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
-//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            //设置状态栏颜色
-            window.setStatusBarColor(getColor(R.color.color_tranparent_dark));
-
+            updateStatusBarColorForM(false);
         }
+        StatusBarUtil.setImmersionMode(this,false);
     }
 
-    private void changeToWhite() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            final Window window = getWindow();
-            //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
-//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            //设置状态栏颜色
-            window.setStatusBarColor(getColor(android.R.color.white));
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void updateStatusBarColorForM(boolean isDark) {
+        final Window window = getWindow();
+        //取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏颜色
+        window.setStatusBarColor(ContextCompat.getColor(this, isDark ? android.R.color.black : android.R.color.white));
 
+        int ui = window.getDecorView().getSystemUiVisibility();
+        if (isDark) {
+            ui &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        } else {
+            ui |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        }
+
+        window.getDecorView().setSystemUiVisibility(ui);
+    }
+
+
+    @TargetApi(19)
+    protected void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
+    }
+
+    public void setStatusBarDarkMode(boolean darkmode, Activity activity) {
+        Class<? extends Window> clazz = activity.getWindow().getClass();
+        try {
+            int darkModeFlag = 0;
+            Class<?> layoutParams = Class.forName("android.view.MiuiWindowManager$LayoutParams");
+            Field field = layoutParams.getField("EXTRA_FLAG_STATUS_BAR_DARK_MODE");
+            darkModeFlag = field.getInt(layoutParams);
+            Method extraFlagField = clazz.getMethod("setExtraFlags", int.class, int.class);
+            extraFlagField.invoke(activity.getWindow(), darkmode ? darkModeFlag : 0, darkModeFlag);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
