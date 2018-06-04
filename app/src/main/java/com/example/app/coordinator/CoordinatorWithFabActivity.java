c58dcfebb401adfcdc7581c5b09fd99e324874fb
RainFool <rainfool1024@gmail.com>
2018-06-04 18:06:29 +0800

    【UI】【Coordinator】snackbar相关踩坑

--- app/src/main/java/com/example/app/coordinator/CoordinatorWithFabActivity.java
+++ app/src/main/java/com/example/app/coordinator/CoordinatorWithFabActivity.java
@@ -9,14 +9,17 @@ import com.example.app.R;
 
 public class CoordinatorWithFabActivity extends AppCompatActivity {
 
+    View mRootView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_with_fab);
+        mRootView = findViewById(R.id.root_view);
     }
 
     public void onFabClick(View view) {
 
-        Snackbar.make(getWindow().getDecorView(), "Snackbar", Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(mRootView, "Snackbar", Snackbar.LENGTH_SHORT).show();
     }
 }
