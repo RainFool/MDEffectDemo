65cda307709e58216a41a351b9577f0761bfc3a9
RainFool <rainfool1024@gmail.com>
2018-07-24 17:25:16 +0800

    完成编译时注解架构

---
+++ processor/src/main/java/push/rainfool/com/processor/BindProcessor.java
@@ -0,0 +1,47 @@
+package push.rainfool.com.processor;
+
+import com.google.auto.service.AutoService;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+import push.rainfool.com.anotation.BindAnnotation;
+
+/**
+ * @author rainfool
+ */
+@AutoService(Processor.class)
+public class BindProcessor extends AbstractProcessor {
+
+    private Messager mMessager;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+        System.out.println("rainfool,init");
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        System.out.println("-------------------rainfool,process");
+//        mMessager.printMessage(Diagnostic.Kind.NOTE,"-------------------rainfool,process");
+        return true;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+
+        HashSet<String> supportTypes = new LinkedHashSet<>();
+        supportTypes.add(BindAnnotation.class.getCanonicalName());
+        return supportTypes;
+    }
+}
