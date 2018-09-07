33a571f39698f1585d38acda60956b48a1ea950a
RainFool <rainfool1024@gmail.com>
2018-09-08 07:27:43 +0800

    【Kotlin】kotlin demo 请理性提交

--- app/src/main/java/com/example/kotlin/KotlinDemo.kt
+++ app/src/main/java/com/example/kotlin/KotlinDemo.kt
@@ -6,5 +6,8 @@ package com.example.kotlin
  * @author rainfool
  * @date 2018/8/22
  */
-class KotlinDemo {
+class KotlinDemo private constructor(int: Int) {
+    companion object {
+        fun getInstance(int: Int) = KotlinDemo(int)
+    }
 }
