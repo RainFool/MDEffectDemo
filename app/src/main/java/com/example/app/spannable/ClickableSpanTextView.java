9b77fec1f77399b375bdea798be683faf68a51c3
RainFool <rainfool1024@gmail.com>
2018-10-17 16:23:24 +0800

    oppo行距处理测试

---
+++ app/src/main/java/com/example/app/spannable/ClickableSpanTextView.java
@@ -0,0 +1,96 @@
+package com.example.app.spannable;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.AppCompatTextView;
+import android.text.Layout;
+import android.text.SpannedString;
+import android.text.style.ClickableSpan;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * No pains, no gains.
+ * <p>
+ * Created by Rod on 2017/11/25.
+ */
+
+public class ClickableSpanTextView extends AppCompatTextView {
+    private ClickableSpan mClickableSpan;
+
+    public ClickableSpanTextView(Context context) {
+        this(context,null);
+    }
+
+    public ClickableSpanTextView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public ClickableSpanTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            setLayoutDirection(LAYOUT_DIRECTION_LTR);
+            setTextDirection(TEXT_DIRECTION_LTR);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        CharSequence text = getText();
+        if (!(text instanceof SpannedString)) {
+            return super.onTouchEvent(event);
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mClickableSpan = getClickableSpan(event);
+                updateClickSpanState(true);
+                return mClickableSpan != null || super.onTouchEvent(event);
+            case MotionEvent.ACTION_MOVE:
+                return mClickableSpan != null || super.onTouchEvent(event);
+            case MotionEvent.ACTION_UP:
+                if (mClickableSpan != null) {
+                    if (mClickableSpan == getClickableSpan(event)) {
+                        mClickableSpan.onClick(this);
+                    }
+                    updateClickSpanState(false);
+                    return true;
+                } else {
+                    return super.onTouchEvent(event);
+                }
+            case MotionEvent.ACTION_CANCEL:
+            default:
+                if (mClickableSpan != null) {
+                    updateClickSpanState(false);
+                    mClickableSpan = null;
+                }
+                return super.onTouchEvent(event);
+        }
+    }
+
+    private void updateClickSpanState(boolean pressed) {
+        if (mClickableSpan instanceof CustomClickableSpan) {
+            ((CustomClickableSpan) mClickableSpan).setPressed(pressed);
+            invalidate();
+        }
+    }
+
+    private ClickableSpan getClickableSpan(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        x -= getTotalPaddingLeft();
+        y -= getTotalPaddingTop();
+
+        x += getScrollX();
+        y += getScrollY();
+
+        Layout layout = getLayout();
+        int line = layout.getLineForVertical(y);
+        int off = layout.getOffsetForHorizontal(line, x);
+
+        ClickableSpan[] link = ((SpannedString) getText()).getSpans(off, off, ClickableSpan.class);
+
+        return link.length > 0 ? link[0] : null;
+    }
+}
--- app/src/main/java/com/example/app/spannable/SpnnableTestActivity.java
+++ app/src/main/java/com/example/app/spannable/SpnnableTestActivity.java
@@ -1,23 +1,29 @@
 package com.example.app.spannable;
 
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.app.R;
 
 public class SpnnableTestActivity extends AppCompatActivity {
 
+    private static final String TAG = "SpnnableTestActivity";
+
     Button mSpannableButton;
     EditText mEditText;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,16 @@ public class SpnnableTestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_spnnable_test);
         mEditText = (EditText) findViewById(R.id.et_input);
         mSpannableButton = (Button) findViewById(R.id.btn_spannable);
+        mTextView = findViewById(R.id.tv_spannable_test);
+
+        SpannableStringBuilder span = new SpannableStringBuilder(mTextView.getText().toString());
+        span.setSpan(new CustomClickableSpan(ContextCompat.getColor(this, R.color.color_aaaaaa), getResources().getColor(R.color.cardview_dark_background), false) {
+            @Override
+            public void onClick(View widget) {
+                Log.d(TAG, "onClick: ");
+            }
+        }, 0, span.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mTextView.setText(span);
 
         mSpannableButton.setOnClickListener(new View.OnClickListener() {
             @Override
