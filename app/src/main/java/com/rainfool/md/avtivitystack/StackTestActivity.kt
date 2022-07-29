50b92085d2110fed9decfd42b0d9e2352aab928c
krystian <krystian@tencent.com>
2022-07-29 10:50:39 +0800

    测试延迟启动activity

--- app/src/main/java/com/rainfool/md/avtivitystack/StackTestActivity.kt
+++ app/src/main/java/com/rainfool/md/avtivitystack/StackTestActivity.kt
@@ -4,39 +4,78 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import com.rainfool.md.R
 
 class StackTestActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "StackTestActivity"
+    }
+
     lateinit var mHandler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stack_test)
-
+        Log.i(TAG, "onCreate: ")
         mHandler = Handler(Looper.getMainLooper())
 
-        mHandler.postDelayed({skipA()},200)
-
         val btn = findViewById<Button>(R.id.btn_skip)
-        btn.setOnClickListener { _ -> skipActivity() }
+        btn.setOnClickListener { skipActivity() }
+
+        val btnFinish = findViewById<Button>(R.id.btn_finish)
+        btnFinish.setOnClickListener {
+            finishSelf()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.i(TAG, "onNewIntent: ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: ")
     }
 
     private fun skipActivity() {
-
-
-        fun skipB() {
-            val intent = Intent(this, StackBActivity::class.java)
-            startActivity(intent)
-        }
-
-        skipA()
+        Log.i(TAG, "skip A Activity clicked")
+        mHandler.postDelayed({ skipA() }, 2000)
     }
 
-    fun skipA() {
+    private fun skipA() {
+        Log.i(TAG, "skip A Activity")
         val intent = Intent(this, StackAActivity::class.java)
         startActivity(intent)
+//        mHandler.post { finish() }
+    }
+
+    private fun finishSelf() {
+        Log.i(TAG, "finishSelf clicked")
+        mHandler.postDelayed({ finish() }, 2000)
     }
 }
---
+++ app/src/main/java/com/rainfool/md/constraintlayout/TestConstraintActivity.kt
@@ -0,0 +1,12 @@
+package com.rainfool.md.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rainfool.md.R
+
+class TestConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_constraint)
+    }
+}
