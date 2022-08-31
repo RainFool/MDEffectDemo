a48fafb1fad16325095b0e56d725ba10f891ba66
krystian <krystian@tencent.com>
2022-08-31 14:34:56 +0800

    建立json test 页面

---
+++ app/src/main/java/com/rainfool/json/JsonTestActivity.kt
@@ -0,0 +1,12 @@
+package com.rainfool.json
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rainfool.md.R
+
+class JsonTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_json_test)
+    }
+}
