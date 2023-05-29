32e3dd38c24bc97e08159753470c38ba4a5b2129
krystian <krystian@tencent.com>
2023-05-29 18:31:54 +0800

    text view 测试

--- app/src/main/java/com/rainfool/md/textview/TextViewTestActivity.kt
+++ app/src/main/java/com/rainfool/md/textview/TextViewTestActivity.kt
@@ -3,7 +3,9 @@ package com.rainfool.md.textview
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.rainfool.md.R
+import kotlinx.android.synthetic.main.activity_drawing_cache_demo.*
 import kotlinx.android.synthetic.main.activity_text_view_test.*
 import kotlin.system.measureTimeMillis
 
@@ -20,28 +22,28 @@ class TextViewTestActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_view_test)
+
+
+        mTestTextView.maxLines = 2
+        mTestTextView.ellipsize = android.text.TextUtils.TruncateAt.END
         mBtnSetNegativeOne.setOnClickListener {
-            testStart()
-//            testSetBaseProp()
+            mTestTextView.text = "joigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioew"
+            mTestTextView.measure(0, 0)
+
+            addViewToContainer()
+
         }
     }
 
-    private fun testStart() {
-        val timeSetText = measureTimeMillis { testSetText() }
+    private fun addViewToContainer() {
+        val view = TextView(this)
+        view.text = "joigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioewjoigjeiwjgioew"
 
-        val timeSetBaseProp = measureTimeMillis { testSetBaseProp() }
+        view.maxLines = 2
+        view.ellipsize = android.text.TextUtils.TruncateAt.END
+        view.measure(0, 0)
 
-
-        Log.i(TAG, "testSetText takes time:$timeSetText ms")
-        Log.i(TAG, "timeSetBaseProp takes time:$timeSetBaseProp ms")
-    }
-
-    private fun testSetText() {
-        mTestTextView.setText("joigjeiwjgioew")
-    }
-
-    private fun testSetBaseProp() {
-        TextViewParams().bindViewInner(mTestTextView)
+        container.addView(view)
     }
 
 }
