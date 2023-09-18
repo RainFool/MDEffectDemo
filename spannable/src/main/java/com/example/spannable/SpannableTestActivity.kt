4c9120bd6b0f909a79a9673e437508b038786a37
krystian <krystian@tencent.com>
2023-09-18 17:29:25 +0800

    加一个模块

---
+++ spannable/src/main/java/com/example/spannable/SpannableTestActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spannable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpannableTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spannable_test)
+    }
+}
